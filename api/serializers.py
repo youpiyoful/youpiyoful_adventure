39e65a62199f96650f4f5346f27d7afe1c19b5ff
youpiyoful <yoanfornari@gmail.com>
2021-03-12 03:42:00 +0100

    init serializers for post and user class

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db.models import fields
+from rest_framework import serializers
+
+from api.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Post
+        fields = ["id", "title", "body", "owner"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "posts"]
