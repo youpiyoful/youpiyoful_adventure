7846804131e62c51d564e93b88b9ab12d7cb553e
youpiyoful <yoanfornari@gmail.com>
2021-03-12 03:39:41 +0100

    init api models with Post class

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+"""mapping of tables for api django app"""
+from django.db.models.deletion import CASCADE
+
+
+class Post(models.Model):
+    """Model of table post"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default="")
+    body = models.TextField(blank=True, default="")
+    owner = models.ForeignKey(
+        "auth.User", related_name="posts", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created"]
