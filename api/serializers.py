fe46b72cf339523f5325a722bfc2805cc05bb136
youpiyoful <yoanfornari@gmail.com>
2021-03-14 07:56:41 +0100

    add comments to PostSerializer and UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,20 +2,22 @@ from django.contrib.auth.models import User
 from django.db.models import fields
 from rest_framework import serializers
 
-from api.models import Post
+from api.models import Comment, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ["id", "title", "body", "owner"]
+        fields = ["id", "title", "body", "owner", "comments"]
 
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "posts"]
+        fields = ["id", "username", "posts", "comments"]
