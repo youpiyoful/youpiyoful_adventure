93c294b9aabf605ace7b1d138b372e7587e772c6
youpiyoful <yoanfornari@gmail.com>
2021-03-14 07:08:59 +0100

    add create, read, update, and delete about post (views + url path)

--- api/views.py
+++ api/views.py
@@ -3,13 +3,36 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from api import serializers
+from api.models import Post
 
 
 class UserList(generics.ListAPIView):
+    """view for get data about all users"""
+
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
+    """view for get data about a specific user"""
+
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    """view for get and create for a list of posts"""
+
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        """Allows the creation of a post"""
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """view for get, update and delete a specific post"""
+
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
